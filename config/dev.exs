16e5f59790b56888bc0e7d87d5741ed5fba0259f
Andrew Shu <talklittle@gmail.com>
2016-10-19 15:06:16 -0700

    ADM: dev config

--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,9 @@
 use Mix.Config
 
-config :pigeon, 
+config :pigeon,
   apns_mode: :dev,
   apns_cert: System.get_env("APNS_CERT"),
   apns_key: System.get_env("APNS_CERT_KEY"),
-  gcm_key: System.get_env("GCM_KEY")
+  gcm_key: System.get_env("GCM_KEY"),
+  adm_client_id: System.get_env("ADM_OAUTH2_CLIENT_ID"),
+  adm_client_secret: System.get_env("ADM_OAUTH2_CLIENT_SECRET")
