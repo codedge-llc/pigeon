3d36b1d7fa341e9c4ce02ff338f2bf37c81747d9
Henry Popp <henry@codedge.io>
2017-05-17 10:06:04 -0500

    chore: bumped to v0.13.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pigeon.Mixfile do
   def project do
     [app: :pigeon,
      name: "Pigeon",
-     version: "0.12.1",
+     version: "0.13.0",
      elixir: "~> 1.2",
      source_url: "https://github.com/codedge-llc/pigeon",
      description: description(),
