bc0235b163ab6faae7b9513c7f5d7d1aba33e067
Henry Popp <henry@codedge.io>
2017-08-01 14:54:29 -0500

    fix: use new kadabra patch for reconnect

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule Pigeon.Mixfile do
   defp deps do
     [{:poison, "~> 2.0 or ~> 3.0"},
     {:httpoison, "~> 0.7"},
-    {:kadabra, "~> 0.2.0", optional: true},
+    {:kadabra, "~> 0.2.1", optional: true},
     {:dogma, "~> 0.1", only: :dev},
     {:earmark, "~> 1.0", only: :dev},
     {:ex_doc, "~> 0.2", only: :dev},
