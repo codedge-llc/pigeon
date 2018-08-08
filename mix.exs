4a9b7ce0b80130ae9a09f7c3ce74cc9a652460ab
Adam Vaughan <adamjvaughan@gmail.com>
2018-08-08 09:02:13 -0600

    Fix warning in mix.exs about quotes.

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Pigeon.Mixfile do
       start_permanent: Mix.env == :prod,
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
-        "coveralls": :test,
+        coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test
