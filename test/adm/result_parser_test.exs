9d0cb2542403dcbd83c40d4086ee0195516de28d
Andrew Shu <talklittle@gmail.com>
2018-09-17 11:31:28 -0700

    fix: ADM enumerate known error values (#129)
    * test: parse known ADM errors
    
    expose crash at String.to_existing_atom/1
    
    * ADM: enumerate known error values
    
    avoid crash at String.to_existing_atom/1

--- test/adm/result_parser_test.exs
+++ test/adm/result_parser_test.exs
@@ -1,4 +1,20 @@
 defmodule Pigeon.ADM.ResultParserTest do
   use ExUnit.Case
   doctest Pigeon.ADM.ResultParser, import: true
+
+  test "parses known error reasons without crashing" do
+    n = Pigeon.ADM.Notification.new("test")
+    onr = fn _ -> :ok end
+
+    Pigeon.ADM.ResultParser.parse(n, %{"reason" => "InvalidRegistrationId"}, onr)
+    Pigeon.ADM.ResultParser.parse(n, %{"reason" => "InvalidData"}, onr)
+    Pigeon.ADM.ResultParser.parse(n, %{"reason" => "InvalidConsolidationKey"}, onr)
+    Pigeon.ADM.ResultParser.parse(n, %{"reason" => "InvalidExpiration"}, onr)
+    Pigeon.ADM.ResultParser.parse(n, %{"reason" => "InvalidChecksum"}, onr)
+    Pigeon.ADM.ResultParser.parse(n, %{"reason" => "InvalidType"}, onr)
+    Pigeon.ADM.ResultParser.parse(n, %{"reason" => "Unregistered"}, onr)
+    Pigeon.ADM.ResultParser.parse(n, %{"reason" => "AccessTokenExpired"}, onr)
+    Pigeon.ADM.ResultParser.parse(n, %{"reason" => "MessageTooLarge"}, onr)
+    Pigeon.ADM.ResultParser.parse(n, %{"reason" => "MaxRateExceeded"}, onr)
+  end
 end
