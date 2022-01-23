c2b9841ce9e0824210e0cb7716d0ea0c4380406f
Henry Popp <henry@codedge.io>
2022-01-23 10:41:10 -0600

    feat: add CI workflow

--- config/test.exs
+++ config/test.exs
@@ -4,8 +4,8 @@ config :pigeon, :test,
   fcm_key: System.get_env("GCM_KEY"),
   valid_fcm_reg_id: System.get_env("VALID_GCM_REG_ID"),
   valid_apns_token: System.get_env("VALID_APNS_TOKEN"),
-  apns_cert: File.read!("cert.pem"),
-  apns_key: File.read!("key_unencrypted.pem"),
+  apns_cert: System.get_env("APNS_CERT"),
+  apns_key: System.get_env("APNS_KEY_UNENCRYPTED"),
   apns_topic: System.get_env("APNS_TOPIC")
 
 config :pigeon, PigeonTest.ADM,
@@ -15,13 +15,13 @@ config :pigeon, PigeonTest.ADM,
 
 config :pigeon, PigeonTest.APNS,
   adapter: Pigeon.APNS,
-  cert: File.read!("cert.pem"),
-  key: File.read!("key_unencrypted.pem"),
+  cert: System.get_env("APNS_CERT"),
+  key: System.get_env("APNS_KEY_UNENCRYPTED"),
   mode: :dev
 
 config :pigeon, PigeonTest.APNS.JWT,
   adapter: Pigeon.APNS,
-  key: File.read!("AuthKey.p8"),
+  key: System.get_env("APNS_AUTH_KEY_P8"),
   key_identifier: System.get_env("APNS_JWT_KEY_IDENTIFIER"),
   team_id: System.get_env("APNS_JWT_TEAM_ID"),
   mode: :dev
@@ -33,6 +33,6 @@ config :pigeon, PigeonTest.LegacyFCM,
 config :pigeon, PigeonTest.FCM,
   adapter: Pigeon.FCM,
   project_id: System.get_env("FCM_PROJECT"),
-  service_account_json: File.read!("service-account.json")
+  service_account_json: System.get_env("FCM_SERVICE_ACCOUNT_JSON")
 
 config :pigeon, PigeonTest.Sandbox, adapter: Pigeon.Sandbox
