20a5ef7318b19aa076f1515fd9182ceb2425e0db
Chris Bell <cjbell1988@gmail.com>
2022-02-28 09:18:25 -0800

    fix: ensure unregistered tokens are marked as such

--- lib/pigeon/fcm/error.ex
+++ lib/pigeon/fcm/error.ex
@@ -11,9 +11,12 @@ defmodule Pigeon.FCM.Error do
     |> parse_response()
   end
 
+  # An unregistered token will always be sent as a `NOT_FOUND` error with a 404
+  @unregistered_statuses ["NOT_FOUND", "UNREGISTERED"]
+
   defp parse_response("UNSPECIFIED_ERROR"), do: :unspecified_error
   defp parse_response("INVALID_ARGUMENT"), do: :invalid_argument
-  defp parse_response("UNREGISTERED"), do: :unregistered
+  defp parse_response(status) when status in @unregistered_statuses, do: :unregistered
   defp parse_response("SENDER_ID_MISMATCH"), do: :sender_id_mismatch
   defp parse_response("QUOTA_EXCEEDED"), do: :quota_exceeded
   defp parse_response("UNAVAILABLE"), do: :unavailable
