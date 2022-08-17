53f7d744b09dbec31bc18d6b245698097b6f21e8
Andrew Timberlake <andrew@andrewtimberlake.com>
2022-08-17 10:05:51 +0200

    Move token generation into Pigeon.APNS.Token
    This means the token is only generated once and used across all workers

--- lib/pigeon/apns/token.ex
+++ lib/pigeon/apns/token.ex
@@ -1,5 +1,11 @@
 defmodule Pigeon.APNS.Token do
   @moduledoc false
+  import Joken.Config
+
+  alias Pigeon.APNS.JWTConfig
+
+  # seconds - 10 seconds short of one hour
+  @token_max_age 3_590
 
   @type t :: {non_neg_integer(), binary() | nil}
 
@@ -18,13 +24,36 @@ defmodule Pigeon.APNS.Token do
     }
   end
 
-  @spec get(String.t()) :: t
-  def get(name) do
-    Agent.get(__MODULE__, &Map.get(&1, name, {0, nil}))
+  @spec get(JWTConfig.t()) :: t
+  def get(%JWTConfig{} = config) do
+    token_storage_key = config.key_identifier <> ":" <> config.team_id
+
+    Agent.get_and_update(__MODULE__, fn map ->
+      {timestamp, saved_token} = Map.get(map, token_storage_key, {0, nil})
+      now = :os.system_time(:seconds)
+
+      age = now - timestamp
+      if age < @token_max_age do
+        {saved_token, map}
+      else
+        token = generate_apns_jwt(config)
+        {token, Map.put(map, token_storage_key, {now, token})}
+      end
+    end)
   end
 
-  @spec update(String.t(), t) :: :ok
-  def update(name, token) do
-    Agent.update(__MODULE__, &Map.put(&1, name, token))
+  @spec generate_apns_jwt(JWTConfig.t()) :: binary
+  defp generate_apns_jwt(config) do
+    key = %{"pem" => config.key}
+    now = :os.system_time(:seconds)
+
+    signer =
+      Joken.Signer.create("ES256", key, %{"kid" => config.key_identifier})
+
+    {:ok, token, _claims} =
+      default_claims(iss: config.team_id, iat: now)
+      |> Joken.generate_and_sign(%{}, signer)
+
+    token
   end
 end
