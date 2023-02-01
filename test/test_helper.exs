e721c10a243aa7b75bfca0e5fbb46b8069d360cd
Peter Mueller <peter.kh.mueller@gmail.com>
2023-02-01 15:50:04 -0500

    feat: support for Goth modules for FCM

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,13 @@
 ExUnit.start(capture_log: true)
 
+fcm_credentials =
+  System.fetch_env!("FCM_SERVICE_ACCOUNT_FILE")
+  |> File.read!()
+  |> Jason.decode!()
+  |> Map.fetch!("source_credentials")
+
 workers = [
+  {Goth, name: PigeonTest.Goth, source: {:refresh_token, fcm_credentials}},
   PigeonTest.ADM,
   PigeonTest.APNS,
   PigeonTest.APNS.JWT,
