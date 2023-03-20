757e948cd7992890b7cfe06dd5a83614b8ee076e
John Hossler <john.m.hossler@gmail.com>
2023-03-19 21:52:30 -0700

    feat: implement a pushy push notification adapter

---
+++ lib/pigeon/pushy.ex
@@ -0,0 +1,105 @@
+defmodule Pigeon.Pushy do
+  @moduledoc """
+  `Pigeon.Adapter` for Pushy pushy notifications
+  """
+
+  defstruct config: nil,
+            queue: Pigeon.NotificationQueue.new(),
+            refresh_before: 5 * 60,
+            socket: nil
+
+  @behaviour Pigeon.Adapter
+
+  alias Pigeon.{Configurable, NotificationQueue}
+  alias Pigeon.Http2.{Client, Stream}
+
+  @impl true
+  def init(opts) do
+    config = Pigeon.Pushy.Config.new(opts)
+    Configurable.validate!(config)
+
+    state = %__MODULE__{config: config}
+
+    with {:ok, socket} <- connect_socket(config) do
+      Configurable.schedule_ping(config)
+      {:ok, %{state | socket: socket}}
+    else
+      {:error, reason} -> {:stop, reason}
+    end
+  end
+
+  @impl true
+  def handle_push(notification, state) do
+    %{config: config, queue: queue} = state
+    headers = Configurable.push_headers(config, notification, [])
+    payload = Configurable.push_payload(config, notification, [])
+
+    Client.default().send_request(state.socket, headers, payload)
+
+    new_q = NotificationQueue.add(queue, state.stream_id, notification)
+
+    state =
+      state
+      |> inc_stream_id()
+      |> Map.put(:queue, new_q)
+
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info(:ping, state) do
+    Client.default().send_ping(state.socket)
+    Configurable.schedule_ping(state.config)
+
+    {:noreply, state}
+  end
+
+  def handle_info({:closed, _}, %{config: config} = state) do
+    case connect_socket(config) do
+      {:ok, socket} ->
+        Configurable.schedule_ping(config)
+
+        state =
+          state
+          |> reset_stream_id()
+          |> Map.put(:socket, socket)
+
+        {:noreply, state}
+
+      {:error, reason} ->
+        {:stop, reason}
+    end
+  end
+
+  def handle_info(msg, state) do
+    case Client.default().handle_end_stream(msg, state) do
+      {:ok, %Stream{} = stream} -> process_end_stream(stream, state)
+      _else -> {:noreply, state}
+    end
+  end
+
+  @doc false
+  def process_end_stream(%Stream{id: stream_id} = stream, state) do
+    %{queue: queue, config: config} = state
+
+    case NotificationQueue.pop(queue, stream_id) do
+      {nil, new_queue} ->
+        # Do nothing if no queued item for stream
+        {:noreply, %{state | queue: new_queue}}
+
+      {notif, new_queue} ->
+        Configurable.handle_end_stream(config, stream, notif)
+        {:noreply, %{state | queue: new_queue}}
+    end
+  end
+
+  @doc false
+  def inc_stream_id(%{stream_id: stream_id} = state) do
+    %{state | stream_id: stream_id + 2}
+  end
+
+  @doc false
+  def reset_stream_id(state) do
+    %{state | stream_id: 1}
+  end
+end
---
+++ lib/pigeon/pushy/config.ex
@@ -0,0 +1,162 @@
+defmodule Pigeon.Pushy.Config do
+  @moduledoc false
+
+  defstruct key: nil,
+            port: 443,
+            uri: nil
+
+  @typedoc ~S"""
+  Pushy configuration struct
+
+  This struct should not be set directly. Instead, use `new/1`
+  with `t:config_opts/0`.
+
+  ## Examples
+
+      %Pigeon.Pushy.Config{
+        key: "some-secret-key",
+        uri: "api.pushy.me",
+        port: 443
+      }
+  """
+  @type t :: %__MODULE__{
+          key: binary | nil,
+          uri: binary | nil,
+          port: pos_integer
+        }
+
+  @typedoc ~S"""
+  Options for configuring Pushy connections.
+
+  ## Configuration Options
+  - `:key` - Pushy secrety key.
+  - `:uri` - Pushy server uri.
+  - `:port` - Push server port. Can be any value, but Pushy only accepts
+    `443`
+  """
+  @type config_opts :: [
+          key: binary,
+          uri: binary,
+          port: pos_integer
+        ]
+
+  @doc false
+  def default_name, do: :pushy_default
+
+  @doc ~S"""
+  Returns a new `Pushy.Config` with given `opts`.
+
+  ## Examples
+
+      iex> Pigeon.Pushy.Config.new(
+      ...>   key: System.get_env("PUSHY_SECRET_KEY"),
+      ...>   uri: "api.pushy.me",
+      ...>   port: 443
+      ...> )
+      %Pigeon.Pushy.Config{
+        key: System.get_env("PUSHY_SECRET_KEY")
+        port: 443,
+        uri: "api.pushy.me"
+      }
+  """
+  def new(opts) when is_list(opts) do
+    %__MODULE__{
+      key: opts |> Keyword.get(:key),
+      uri: Keyword.get(opts, :uri, 'api.pushy.me'),
+      port: Keyword.get(opts, :port, 443)
+    }
+  end
+end
+
+defimpl Pigeon.Configurable, for: Pigeon.Pushy.Config do
+  @moduledoc false
+
+  require Logger
+
+  import Pigeon.Tasks, only: [process_on_response: 1]
+
+  alias Pigeon.Encodable
+  alias Pigeon.Pushy.{Config, Error}
+
+  @type sock :: {:sslsocket, any, pid | {any, any}}
+
+  # Configurable Callbacks
+
+  @spec connect(any) :: {:ok, sock} | {:error, String.t()}
+  def connect(%Config{uri: uri} = config) do
+    case connect_socket_options(config) do
+      {:ok, options} ->
+        Pigeon.Http2.Client.default().connect(uri, :https, options)
+    end
+  end
+
+  def connect_socket_options(config) do
+    opts =
+      [
+        {:active, :once},
+        {:packet, :raw},
+        {:reuseaddr, true},
+        {:alpn_advertised_protocols, [<<"h2">>]},
+        :binary
+      ]
+      |> add_port(config)
+
+    {:ok, opts}
+  end
+
+  def add_port(opts, %Config{port: 443}), do: opts
+  def add_port(opts, %Config{port: port}), do: [{:port, port} | opts]
+
+  def push_headers(
+        %Config{key: key},
+        _notification,
+        _opts
+      ) do
+    [
+      {":method", "POST"},
+      {":path", "/push/?api_key=#{key}"},
+      {"content-type", "application/json"},
+      {"accept", "application/json"}
+    ]
+  end
+
+  def push_payload(_config, notification, _opts) do
+    Encodable.binary_payload(notification)
+  end
+
+  def handle_end_stream(_config, %{error: nil} = stream, notif) do
+    stream.body
+    |> Pigeon.json_library().decode!()
+    |> case do
+      %{"name" => name} ->
+        notif
+        |> Map.put(:name, name)
+        |> Map.put(:response, :success)
+        |> process_on_response()
+
+      %{"error" => error} ->
+        notif
+        |> Map.put(:error, error)
+        |> Map.put(:response, Error.parse(error))
+        |> process_on_response()
+    end
+  end
+
+  def schedule_ping(_config), do: :ok
+
+  def close(_config) do
+  end
+
+  def validate!(_config), do: :ok
+
+  @doc false
+  def redact(config) when is_map(config) do
+    [:key]
+    |> Enum.reduce(config, fn key, acc ->
+      case Map.get(acc, key) do
+        val when is_map(val) -> Map.put(acc, key, "[FILTERED]")
+        _ -> acc
+      end
+    end)
+  end
+end
