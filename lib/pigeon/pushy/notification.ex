92b0ecb600320604bc5abeded730f2ee44626772
John Hossler <john.m.hossler@gmail.com>
2023-03-29 12:32:31 -0700

    docs(pushy): add documentation for notification and pushy module

--- lib/pigeon/pushy/notification.ex
+++ lib/pigeon/pushy/notification.ex
@@ -1,6 +1,9 @@
 defmodule Pigeon.Pushy.Notification do
   @moduledoc """
   Defines Pushy notification struct and convenience constructor functions.
+
+  For more information on Pushy notification requests, see
+  https://pushy.me/docs/api/send-notifications.
   """
 
   defstruct __meta__: %Pigeon.Metadata{},
@@ -18,6 +21,27 @@ defmodule Pigeon.Pushy.Notification do
             successful_device_count: nil,
             failed: nil
 
+  @typedoc """
+  Pushy notification
+
+  ## Examples
+      %Pigeon.Pushy.Notification{
+          __meta__: %Pigeon.Metadata{on_response: nil},
+          to: "device token or topic",
+          data: %{"message" => "hello world"},
+          time_to_live: nil,
+          content_available: nil,
+          mutable_content: nil,
+          notification: nil,
+          schedule: nil,
+          collapse_key: nil,
+          response: nil, # Set on push response
+          success: nil, # Set on push response
+          push_id: nil, # Set on push response
+          successful_device_count: nil, # Set on push response
+          failed: nil # Set on push response
+      }
+  """
   @type t :: %__MODULE__{
           __meta__: Pigeon.Metadata.t(),
           to: String.t() | [String.t()],
@@ -28,12 +52,25 @@ defmodule Pigeon.Pushy.Notification do
           notification: map | nil,
           schedule: integer | nil,
           collapse_key: String.t() | nil,
-          response: atom | nil,
+          response: response,
           push_id: String.t() | nil,
           success: boolean() | nil,
           successful_device_count: integer() | nil,
           failed: [String.t()] | nil
         }
+
+  @typedoc """
+  Pushy push response
+
+  - nil - Push has not been sent yet
+  - `:success` - Push was successfully sent.
+  - `:failure` - If we don't receive an error code, but the response is not successful,
+     this message is returned.
+  - `t:Pigeon.Pushy.Error.error_response/0` - Push attempted
+     server responded with error.
+  - `:timeout` - Internal error. Push did not reach Pushy servers.
+  """
+  @type response :: nil | :success | :failure | error_response | :timeout
 
   @type error_response ::
           :no_recipients
@@ -47,6 +84,32 @@ defmodule Pigeon.Pushy.Notification do
           | :internal_server_error
           | :unknown_error
 
+  @doc """
+  Returns a `Pushy.Notification` struct with the given message and destination.
+
+  A destination could be either a single destination, or a list of them. A destination
+  is either a device token OR a topic (prefix topic names with '/topics/').
+
+  ## Examples
+
+      iex> Pigeon.Pushy.Notification.new(%{"message" => "Hello, world!"}, "device token")
+      %Pigeon.APNS.Notification{
+        __meta__: %Pigeon.Metadata{},
+        to: "device token",
+        data: %{"message" => "Hello, world!"},
+        time_to_live: nil,
+        content_available: nil,
+        mutable_content: nil,
+        notification: nil,
+        schedule: nil,
+        collapse_key: nil,
+        response: nil,
+        success: nil,
+        push_id: nil,
+        successful_device_count: nil,
+        failed: nil
+    }
+  """
   @spec new(map, String.t() | [String.t()]) :: __MODULE__.t()
   def new(message, device_ids) do
     %__MODULE__{
@@ -55,67 +118,97 @@ defmodule Pigeon.Pushy.Notification do
     }
   end
 
+  @doc """
+  Adds or updates the `data` field in the notification.
+
+  ## Examples
+
+      iex> Pigeon.Pushy.Notification.put_data(notification, %{"message" => "some message"})
+      %Pigeon.Pushy.Notification{... data: %{"message" => "some message"} ...}
+  """
+  @spec put_data(__MODULE__.t(), map()) :: __MODULE__.t()
+  def put_data(notification, data) do
+    %{notification | data: data}
+  end
+
+  @doc """
+  Adds or updates the `time_to_live` field in the notification.
+
+  This is how long (in seconds) the push notification should be kept if the device is offline.
+  If unspecified, notifications will be kept for 30 days. The maximum value is 365 days.
+
+  ## Examples
+
+      iex> Pigeon.Pushy.Notification.put_time_to_live(notification, 3600)
+      %Pigeon.Pushy.Notification{... time_to_live: 3600 ...}
+  """
   @spec put_time_to_live(__MODULE__.t(), integer()) :: __MODULE__.t()
   def put_time_to_live(notification, time_to_live) do
     %{notification | time_to_live: time_to_live}
   end
 
+  @doc """
+  Adds or updates the `content_available` field in the notification.
+
+  ## Examples
+
+      iex> Pigeon.Pushy.Notification.put_content_available(notification, true)
+      %Pigeon.Pushy.Notification{... content_available: true ...}
+  """
   @spec put_content_available(__MODULE__.t(), boolean()) :: __MODULE__.t()
   def put_content_available(notification, content_available) do
     %{notification | content_available: content_available}
   end
 
+  @doc """
+  Adds or updates the `mutable_content` field in the notification.
+
+  ## Examples
+
+      iex> Pigeon.Pushy.Notification.put_mutable_content(notification, false)
+      %Pigeon.Pushy.Notification{... mutable_content: false ...}
+  """
   @spec put_mutable_content(__MODULE__.t(), boolean()) :: __MODULE__.t()
   def put_mutable_content(notification, mutable_content) do
     %{notification | mutable_content: mutable_content}
   end
 
+  @doc """
+  Adds or updates the `notification` field in the notification.
+
+  ## Examples
+
+      iex> Pigeon.Pushy.Notification.put_notification(notification, %{"title" => "New message", "body" => "Hello"})
+      %Pigeon.Pushy.Notification{... notification: %{"title" => "New message", "body" => "Hello"} ...}
+  """
   @spec put_notification(__MODULE__.t(), map) :: __MODULE__.t()
   def put_notification(notification, notification_details) do
     %{notification | notification: notification_details}
   end
 
+  @doc """
+  Adds or updates the `schedule` field in the notification.
+
+  ## Examples
+
+      iex> Pigeon.Pushy.Notification.put_schedule(notification, 1648686700)
+      %Pigeon.Pushy.Notification{... schedule: 1648686700 ...}
+  """
   @spec put_schedule(__MODULE__.t(), integer) :: __MODULE__.t()
   def put_schedule(notification, schedule) do
     %{notification | schedule: schedule}
   end
 
+  @doc """
+  Adds or updates the `collapse_key` field in the notification.
+
+  ## Examples
+
+      iex> Pigeon.Pushy.Notification.put_collapse_key(notification, "new_message")
+      %Pigeon.Pushy.Notification{... collapse_key: "new_message" ...}
+  """
   @spec put_collapse_key(__MODULE__.t(), String.t()) :: __MODULE__.t()
   def put_collapse_key(notification, collapse_key) do
     %{notification | collapse_key: collapse_key}
-  end
-end
-
-defimpl Pigeon.Encodable, for: Pigeon.Pushy.Notification do
-  def binary_payload(notif) do
-    encode_requests(notif)
-  end
-
-  @doc false
-  def encode_requests(notif) do
-    %{}
-    |> encode_to(notif.to)
-    |> encode_data(notif.data)
-    |> maybe_encode_attr("time_to_live", notif.time_to_live)
-    |> maybe_encode_attr("content_available", notif.content_available)
-    |> maybe_encode_attr("mutable_content", notif.mutable_content)
-    |> maybe_encode_attr("notification", notif.notification)
-    |> maybe_encode_attr("schedule", notif.schedule)
-    |> maybe_encode_attr("collapse_key", notif.collapse_key)
-    |> Pigeon.json_library().encode!()
-  end
-
-  defp encode_to(map, value) do
-    Map.put(map, "to", value)
-  end
-
-  defp encode_data(map, value) do
-    Map.put(map, "data", value)
-  end
-
-  defp maybe_encode_attr(map, _key, nil), do: map
-
-  defp maybe_encode_attr(map, key, val) do
-    Map.put(map, key, val)
   end
 end
