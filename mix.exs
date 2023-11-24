543d19ac7d1a7cb27ca9a413dec3fa3c5d017d4c
Joe Goldin <joe@joegold.in>
2023-11-24 12:52:39 -0800

    fix: drop support for httpoison 0.x in mix.exs (#243)

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule Pigeon.Mixfile do
       {:excoveralls, "~> 0.5", only: :test, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:goth, "~> 1.3.0"},
-      {:httpoison, "~> 0.7 or ~> 1.0 or ~> 2.0"},
+      {:httpoison, "~> 1.0 or ~> 2.0"},
       {:jason, "~> 1.0", optional: true},
       {:joken, "~> 2.1"},
       {:kadabra, "~> 0.6.0"}
